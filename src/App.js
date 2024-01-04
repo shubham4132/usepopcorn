4fd5ae87337c846f204ab42d15ff40165905a36e
shubham4132 <shubhamg3122134@gmail.com>
2024-01-04 21:06:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
+            // `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
@@ -300,7 +301,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          // `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https:////www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         // console.log(data);
