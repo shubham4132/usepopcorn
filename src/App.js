ad69c40df6b1e6e42021d38b0f1dd157f450386d
shubham4132 <shubhamg3122134@gmail.com>
2024-01-04 18:18:02 +0530

    using https in place of http inside the fetch function

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
@@ -300,7 +300,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         // console.log(data);
